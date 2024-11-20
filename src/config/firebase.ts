de7b9168c0c3dfaae9c9fe524dfb40e52c676645
Sheikh <sheikhshafayat2@gmail.com>
2024-11-20 20:10:24 +0900

    updated

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
-  projectId: import.meta.env.VITE_FIREBASE_PRJ,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG,
-  appId: import.meta.env.VITE_FIREBASE_APP,
+  apiKey: "AIzaSyBvx3DMmYOu9I56IOzdpPcbjb_c71L4zYA",
+  authDomain: "fir-tuto-d6343.firebaseapp.com",
+  projectId: "fir-tuto-d6343",
+  storageBucket: "fir-tuto-d6343.firebasestorage.app",
+  messagingSenderId: "190126781323",
+  appId: "1:190126781323:web:b69c0330172683d7d3358e"
 };
 
 const app = initializeApp(firebaseConfig);
